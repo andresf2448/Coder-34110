14b0f5ddb004a04d780e41d19392cc2e8a289b51
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-11-10 21:18:17 -0500

    Clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,61 @@
+//comentario de una sola linea
+
+/* 
+comentario
+de
+varias
+lineas
+*/
+
+// asdfasdf
+// asdfasdfasdfasdf
+// asdf
+
+/* asdfasdf
+asdfasdfasdfasdf
+asdf */
+
+//declaracion de variables
+/* let nombre = "andres"; //recomendable
+var apellido = 'ruiz'; //no es recomendable
+let edad = 25;
+
+console.log("edad", typeof(edad));
+console.log("nombre", typeof(nombre));
+console.log("apellido", typeof(apellido)); */
+
+// const NUMERO = 5;
+// NUMERO = 7;
+
+// let numero = 6;
+// numero = 7;
+
+// let nombreBonitoGrande;
+
+// let numero1 = 5;
+// let numero2 = 6;
+// let resultado = numero1 / numero2;
+
+// console.log(resultado);
+
+/* let saludo = "Hola";
+let nombre = "andres";
+
+let mensajeNormal = saludo + " " + nombre;
+let mensaje = `${saludo} ${nombre} genial`;
+console.log(mensaje); */
+
+// let saludo = "Hola";
+// let nombre = "andres";
+
+// // console.log("Hola nombre como estas");
+// console.log(`Hola ${nombre} como estas`);
+
+// let nombre = prompt("Ingrese su nombre");
+// let mensaje = `Hola ${nombre}`;
+// alert(mensaje);
+
+let numero1 = parseInt(prompt("Ingrese el numero 1"));
+let numero2 = parseInt(prompt("Ingrese el numero 2"));
+
+alert(numero1 + numero2);
