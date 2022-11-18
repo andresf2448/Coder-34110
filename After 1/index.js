350b71627d684e7a3a34910bd364d74b404c1a45
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-11-18 16:52:05 -0500

    After 1

---
+++ After 1/index.js
@@ -0,0 +1,54 @@
+// let cantidad = parseInt(prompt("Ingrese el numero de veces a repetir"));
+
+// for(let i = 0; i < 10; i++){
+//   console.log("hola");
+// }
+
+// nombre("asdfasdf")
+/* let usuario = prompt("Ingrese el usuario");
+
+while(usuario != "andres"){
+  alert("usuario incorrecto");
+
+  usuario = prompt("Ingrese el usuario");
+} */
+
+// let repetir = true;
+
+// while(repetir){
+//   console.log("entramos");
+// }
+
+// do{
+//   console.log("entramos");
+// }while(repetir);
+
+// let entrada = prompt("Ingrese el día");
+
+// while (entrada != "ESC") {
+  // if(entrada === "lunes"){
+  //   console.log("jugar");
+  // }else if(entrada === "martes"){
+  //   console.log("dormir");
+  // }else{
+  //   console.log("ese dia no existe");
+  // }
+
+  // entrada = prompt("Ingrese el día");
+
+  // switch (entrada) {
+  //   case "lunes":
+  //     console.log("jugar");
+  //     break;
+
+  //   case "martes":
+  //     console.log("dormir");
+  //     break;
+
+  //   default:
+  //     console.log("ese dia no existe");
+  //     break;
+  // }
+
+  // entrada = prompt("Ingrese el día");
+// }
