9de020629225dffa6c9a3e3df3c71ba67fd079ad
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-11-29 20:12:12 -0500

    Clase 6

---
+++ Clase 6/index.js
@@ -0,0 +1,59 @@
+// const arreglo1 = [];
+// const numeros = [1, 4, 6, 10, 977];
+// const nombres = ["camila", "andres", "sofi"];
+// const valores = [false, true, true];
+// const variados = ["mariana", true, 4];
+
+// const nombres = ["camila", "andres", "sofi"];
+// console.log(nombres[1]);
+// console.log(nombres[2]);
+
+// const nombres = ["camila", "andres", "sofi", "melisa", "juan"];
+
+// for(let index = 0; index < nombres.length; index++){
+//   console.log(nombres[index]);
+// }
+
+// const nombres = ["camila", "andres", "sofi"];
+// nombres.push("melisa"); //agregamos elementos al final
+// nombres.unshift("juan"); //agregamos elementos al principio
+
+// console.log(nombres);
+
+// const nombres = ["camila", "andres", "sofi"];
+// nombres.pop(); // quitar elementos del final
+// nombres.shift(); // quitar elementos del inicio
+
+// console.log(nombres);
+
+// const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+
+// nombres.splice(1, 1); //para elminar uno o varios elementos de un arreglo
+// console.log(nombres);
+
+// const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+// console.log(nombres.join("***")); //generar un string con todos los elementos separados por el valor pasado por parámetro
+
+// const nombres = ["Rita", "Pedro", "Miguel"];
+// const animales = ["zeus", "max", "firu"];
+// const varios = animales.concat(nombres); //concatena o une los arreglos
+
+// console.log(varios);
+// const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+// const fragmento = nombres.slice(1, 4); // crea una copia de un fragmento dado
+
+// console.log(fragmento);
+// console.log(nombres);
+
+// const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+
+// console.log(nombres.indexOf("Ana"));// obtiene la posicion del elemento en el arreglo
+
+// const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+// console.log(nombres.includes("Pedro"));// saber si un elemento se encuentra dentro del arreglo
+// console.log(nombres.includes("Miguelito"));
+
+const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+nombres.reverse();
+
+console.log(nombres);
