1d8d81928749c8ab9d6e2cf1a31f91d221cb733c
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-12-15 20:05:48 -0500

    Clase 10

---
+++ Clase 10/index.js
@@ -0,0 +1,29 @@
+//setItem guardar un par en el storage
+// localStorage.setItem("nombre", "andres");
+// localStorage.setItem("numero", 5);
+// localStorage.setItem("valor", true);
+
+//getItem recupera un valor del storage
+// let nombre = localStorage.getItem("nombre");
+// console.log(nombre);
+
+// let valor = localStorage.getItem("valor");
+// console.log(valor);
+
+// let numero = localStorage.getItem("numero");
+// console.log(numero);
+
+// localStorage.setItem("objeto", { nombre: "camila", edad: 40 });
+// localStorage.setItem("arreglo", [1, 2, 3, 4, 5]);
+
+// sessionStorage.setItem("edad", 45);
+
+// for (let i = 0; i < localStorage.length; i++) {
+//   let clave = localStorage.key(i);
+//   console.log("clave ", clave);
+//   console.log("valor ", localStorage.getItem(clave));
+// }
+
+//eleminar datos del storage
+// localStorage.removeItem("nombre");
+localStorage.clear();
